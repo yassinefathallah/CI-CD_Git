95cb5bd4dba990cb3139cdc10d3a22fea62334a9
yassinefathallah <92339390+yassinefathallah@users.noreply.github.com>
2024-09-25 12:20:37 +0200

    first commit

---
+++ src/main/java/com/example/cicd_git/CiCdGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicd_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicd_git/CiCdGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
